d951428a2694631f982056a03227e3cd70df6168
bartek <log.mazik.bartek@gmail.com>
2025-02-10 15:17:23 +0100

    redux init

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -5,6 +5,8 @@ import { Navbar } from "@/components/navbar";
 import { Inter as FontSans } from "next/font/google";
 import { cn } from "@/lib/utils";
 import Sidebar from "@/components/sidebar";
+import { wrapper, store } from "../store/store";
+import { Provider } from "react-redux";
 
 import "@/styles/globals.css";
 
@@ -13,28 +15,32 @@ const fontSans = FontSans({
   variable: "--font-sans",
 });
 
-export default function Layout({ children }: { children: ReactNode }) {
+function Layout({ children }: { children: ReactNode }) {
   const [isDark, setIsDark] = useState(false);
 
   return (
     <html lang="en">
       <body className={`${isDark === true ? "dark" : ""}`}>
         <main className="flex flex-col w-full">
-          <Navbar setIsDark={setIsDark} isDark={isDark} />
-          <div className="flex flex-row">
-            <Sidebar />
-            <div
-              className={cn(
-                "max-w-screen-2xl w-full md:ml-48 mt-16",
-                fontSans.variable
-              )}
-            >
-              {children}
+          <Provider store={store}>
+            <Navbar setIsDark={setIsDark} isDark={isDark} />
+            <div className="flex flex-row">
+              <Sidebar />
+              <div
+                className={cn(
+                  "max-w-screen-2xl w-full md:ml-48 mt-16",
+                  fontSans.variable
+                )}
+              >
+                {children}
+              </div>
             </div>
-          </div>
-          {/* <Footer /> */}
+            {/* <Footer /> */}
+          </Provider>
         </main>
       </body>
     </html>
   );
 }
+
+export default wrapper.withRedux(Layout);
